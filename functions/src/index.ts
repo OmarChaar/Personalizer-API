86d6d032186375af3144339741c22200e4c66384
Omar Chaar <oishaar@gmail.com>
2023-03-10 22:44:25 -0300

    using cors and express

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,7 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
+import * as express from 'express';
+import * as cors from 'cors';
 
 admin.initializeApp({
     credential: admin.credential.applicationDefault(),
@@ -7,6 +9,17 @@ admin.initializeApp({
 });
 
 const db = admin.firestore();
+
+const app = express();
+app.use(cors());
+
+// enable CORS for all requests
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+  });
 
 async function findAccount(id: any) {
     const accountRef = db.collection('accounts').doc(id);
@@ -32,7 +45,7 @@ async function findAccount(id: any) {
     };
 }
 
-export const getAccount = functions.https.onRequest(async (req, res) => {
+app.get('/getAccount', async (req, res) => {
     const id = req.query.id;
 
     const result = await findAccount(id);
@@ -42,7 +55,6 @@ export const getAccount = functions.https.onRequest(async (req, res) => {
         res.status(404).send('Account not found');
     }
 });
-
 
 async function findClient(id: any, apartment: any) {
 
@@ -54,7 +66,7 @@ async function findClient(id: any, apartment: any) {
     }
 }
 
-export const getClient = functions.https.onRequest(async (req, res) => {
+app.get('/getClient', async (req, res) => {
     const client = {id: req.query.id, apartment: req.query.apartment};
 
     const result = await findClient(client.id, client.apartment);
@@ -65,7 +77,7 @@ export const getClient = functions.https.onRequest(async (req, res) => {
     }
 });
 
-export const getSections = functions.https.onRequest(async (req, res) => {
+app.get('/getSections', async (req, res) => {
     try {
         const { id } = req.query; 
         const sectionsRef = db.collection(`accounts/${id}/sections`);
@@ -77,6 +89,8 @@ export const getSections = functions.https.onRequest(async (req, res) => {
         res.status(500).send('Internal server error');
     }
 });
+
+export const api = functions.https.onRequest(app);
 
 export const updateClients = functions.firestore
     .document('accounts/{accountId}/clients/{clientId}')
@@ -99,6 +113,3 @@ export const updateClients = functions.firestore
          
         }
 });
-
-
-// firebase deploy --only functions
